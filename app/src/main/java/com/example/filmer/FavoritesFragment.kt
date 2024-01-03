9f4b3cc46fa323d5e88c38fbc9e5ebb07b74f500
redemi <prio73956@gmail.com>
2024-01-03 11:28:28 +0300

    module 27 work

--- app/src/main/java/com/example/filmer/FavoritesFragment.kt
+++ app/src/main/java/com/example/filmer/FavoritesFragment.kt
@@ -1,45 +1,50 @@
 package com.example.filmer
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.filmer.databinding.FragmentHomeBinding
+import com.example.filmer.databinding.FragmentFavoritesBinding
+import java.util.stream.Collectors
 
-class HomeFragment : Fragment(), HasSearch {
-    private lateinit var binding: FragmentHomeBinding
+class FavoritesFragment : Fragment(), HasSearch {
+    private lateinit var binding: FragmentFavoritesBinding
     private lateinit var adapter: RAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyc = binding.RView
+        val recyc = binding.recyclerView
         recyc.layoutManager = LinearLayoutManager(requireContext())
 
         adapter = RAdapter(object : RAdapter.OnItemClickListener {
             override fun click(film: RData) {
                 (requireActivity() as MainActivity).launchDetailsFragment(film)
             }
-        })
+        }, showIsFavorite = false)
 
-        adapter.data = (requireActivity() as MainActivity).getData()
+        val dataForAdapter: ArrayList<RData> = ArrayList()
+        (requireActivity() as MainActivity).getData().filter { it.isFavorite }
+            .forEach(dataForAdapter::add)
+
+        adapter.data = dataForAdapter
         recyc.adapter = adapter
         recyc.addItemDecoration(RItemDecorator(5))
 
-        val callback = RItemInteraction(binding.RView.adapter as RAdapter)
-        val touchHelper = ItemTouchHelper(callback)
-        touchHelper.attachToRecyclerView(binding.RView)
+//        val callback = RItemInteraction(binding.recyclerView.adapter as RAdapter)
+//        val touchHelper = ItemTouchHelper(callback)
+//        touchHelper.attachToRecyclerView(binding.recyclerView)
     }
 
     override fun onQueryTextSubmit(text: String?) {
@@ -53,5 +58,6 @@ class HomeFragment : Fragment(), HasSearch {
         adapter.data = search(text!!, (requireActivity() as MainActivity).getData())
         adapter.notifyDataSetChanged()
     }
+
 
 }
